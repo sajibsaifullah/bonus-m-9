5e9fc1888a37f0767fef3c2ffd4d89b6f854ff5a
sajibsaifullah <dusajibphy@gmail.com>
2023-04-12 16:00:09 +0600

    first commit

---
+++ my-phone-server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require('cors');
+// const phones = require('phones');
+const phones = require("./phones.json");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("my phones information coming sooooooooon");
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('i need data for id ', id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone);
+})
+
+app.listen(port, () => {
+  console.log(`My phone server is running on port: ${port}`);
+});
